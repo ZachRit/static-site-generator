0fa53ae3b9501d835dc49aa654141bf22ae6db4d
Zach Ritter <zachritter@Zachs-MacBook-Pro.local>
2024-04-07 00:12:20 -0400

    convert strings to nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,10 +5,9 @@ import re
 # This function will only support a single level of nesting (one delimiter type)
 # I will probably add support for multiple delimiters some other time
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    #old_nodes is an array of nodes (LeafNodes, ParentNodes, HTMLNodes)
     nodes = []
     for old_node in old_nodes:
-        if not isinstance(old_node, TextNode):
+        if old_node.text_type != text_type_text:
             nodes.append(old_node)
         else:
             split_nodes = old_node.text.split(delimiter)
@@ -81,10 +80,11 @@ def split_nodes_link(old_nodes):
                 nodes.append(TextNode(old_text_copy, text_type_text))
     return nodes
 
-node = TextNode(
-    "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-    text_type_text,
-)
-new_nodes = split_nodes_image([node])
-print(new_nodes)
-
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, '**', text_type_bold)
+    nodes = split_nodes_delimiter(nodes, '*', text_type_italic)
+    nodes = split_nodes_delimiter(nodes, '`', text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
